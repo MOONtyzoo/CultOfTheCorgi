f45a6e52740f42c8ea33b69725aba02176869e8b
Ross Ziegler <rziegler@chapman.edu>
2024-11-17 11:49:03 -0800

    Added Virtual Camera and Player Movement

---
+++ Assets/Scripts/Corgi.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Corgi : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer CorgiSpriteRenderer;
+    
+    public void Move(Vector2 direction)
+    {
+        FaceCorrectDirection(direction);
+        float xAmount = direction.x * 5f * Time.deltaTime;
+        float yAmount = direction.y * 5f * Time.deltaTime;
+        
+        transform.Translate(xAmount, 0, yAmount);
+        
+    }
+    
+    private void FaceCorrectDirection(Vector2 direction)
+    {
+        if (direction.x > 0)
+        {
+            CorgiSpriteRenderer.flipX = false;
+        }
+        else if (direction.x < 0)
+        {
+            CorgiSpriteRenderer.flipX = true;
+        }
+    }
+}
---
+++ Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class KeyboardInput : MonoBehaviour
+{
+    [SerializeField] private Corgi Corgi;
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector2 direction = new Vector2(0, 1);
+            Corgi.Move(direction);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector2 direction = new Vector2(0, -1);
+            Corgi.Move(direction);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector2 direction = new Vector2(-1, 0);
+            Corgi.Move(direction);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector2 direction = new Vector2(1, 0);
+            Corgi.Move(direction);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //dodge
+        }
+        
+    }
+}
