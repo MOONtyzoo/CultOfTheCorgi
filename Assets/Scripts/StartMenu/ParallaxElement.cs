89028642b34339a8690f291ccda89f7c6a3b89fc
Cristian Melgoza <91515108+MOONtyzoo@users.noreply.github.com>
2024-11-19 19:37:30 -0800

    Made the main menu even cooler
    Adds parallax stuff to the things

--- Assets/Scripts/StartMenu/ParallaxElement.cs
+++ Assets/Scripts/StartMenu/ParallaxElement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class ParallaxElement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private Vector2 sensitivity;
+
+    private RectTransform rectTransform;
+    private Vector3 originalPosition;
+
+    void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+        originalPosition = rectTransform.position;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        HandlePositioning();
+    }
+
+    private void HandlePositioning() {
+        Vector3 mouseOffset = GetMouseOffsetFromCenter();
+        Vector3 transformOffset = new Vector3(mouseOffset.x*sensitivity.x, mouseOffset.y*sensitivity.y, 0.0f);
+        Vector3 newPosition = originalPosition - transformOffset;
+        print(newPosition);
+        rectTransform.position = newPosition;
+    }
+
+    private Vector3 GetMouseOffsetFromCenter() {
+        Vector3 center = new Vector3(Screen.width/2f, Screen.height/2f, 0f);
+        return Input.mousePosition - center;
     }
 }
