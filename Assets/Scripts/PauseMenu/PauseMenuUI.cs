bb227b717c40f53e9c0f8bd3e302bdf161f33a30
Cristian Melgoza <91515108+MOONtyzoo@users.noreply.github.com>
2024-11-28 10:51:25 -0800

    Refactor PauseMenuUI to use InputReader

--- Assets/Scripts/PauseMenu/PauseMenuUI.cs
+++ Assets/Scripts/PauseMenu/PauseMenuUI.cs
@@ -6,21 +6,28 @@ using UnityEngine.UI;
 
 public class PauseMenuUI : MonoBehaviour
 {
+   [SerializeField] private InputReader input;
+
    [SerializeField] private CanvasGroup pausePanel;
    [SerializeField] private Button returnToStartScreenButton;
    [SerializeField] private Button resumeButton;
+
+   private bool isPaused = false;
+
    private void Awake()
    {
       HidePauseScreen();
       SetupButtonClickedEvents();
    }
 
-   private void Update()
+   private void OnEnable()
    {
-      if (Input.GetKeyDown(KeyCode.Escape))
-      {
-         ShowPauseScreen();
-      }
+      input.PauseEvent += TogglePause;
+   }
+
+   private void OnDisable()
+   {
+      input.PauseEvent -= TogglePause;
    }
    
    private void SetupButtonClickedEvents() {
@@ -28,19 +35,35 @@ public class PauseMenuUI : MonoBehaviour
       resumeButton.onClick.AddListener(() => HidePauseScreen());
    }
 
+   private void TogglePause() {
+      if (isPaused) {
+         Resume();
+      } else {
+         Pause();
+      }
+   }
+
+   private void Pause() {
+      Time.timeScale = 0.0f;
+      isPaused = true;
+      ShowPauseScreen();
+   }
+   
+   private void Resume() {
+      Time.timeScale = 1.0f;
+      isPaused = false;
+      HidePauseScreen();
+   }
+
    private void ShowPauseScreen() {
       pausePanel.alpha = 1;
       pausePanel.interactable = true;
       pausePanel.blocksRaycasts = true;
-
-      Time.timeScale = 0.0f;
    }
-   
+
    private void HidePauseScreen() {
       pausePanel.alpha = 0;
       pausePanel.interactable = false;
       pausePanel.blocksRaycasts = false;
-
-      Time.timeScale = 1.0f;
    }
 }
